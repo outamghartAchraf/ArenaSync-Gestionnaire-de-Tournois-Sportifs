8607436cdf6155fb3d7c92cba1c2e81e1e4681a9
outamghartAchraf <outamghart2000@gmail.com>
2026-03-03 13:04:24 +0000

    add code for ItemCARD

---
+++ src/components/ParticipantItem.jsx
@@ -0,0 +1,17 @@
+import StatusBadge from './StatusBadge';
+
+export default function ParticipantItem({ participant }) {
+  return (
+    <div className="bg-white rounded-xl p-3 flex flex-col items-center text-center hover:shadow-md transition">
+      <img
+        src={participant.avatar}
+        alt={participant.name}
+        className="w-14 h-14 rounded-full mb-2 object-cover"
+      />
+      <h4 className="text-xs font-semibold text-gray-900 mb-1 line-clamp-2">
+        {participant.name}
+      </h4>
+      <StatusBadge status={participant.status} statusClass={participant.statusClass} />
+    </div>
+  );
+}
