a072f4ccc7ac7f5d242e90dd9f82372b08961b55
outamghartAchraf <outamghart2000@gmail.com>
2026-03-11 11:45:35 +0000

    Change button text depending on state.

--- src/components/RegistrationButton.jsx
+++ src/components/RegistrationButton.jsx
@@ -5,7 +5,7 @@ function RegistrationButton({isRegistered, onClick}) {
       className={`w-full py-3 rounded-lg font-bold transition ${isRegistered ? "bg-gray-500/80 text-gray-300" : "bg-green-500/80 text-white hover:bg-green-600/80 border border-green-400/50"
       }`}
     >
-       Register Now    </button>
+       {isRegistered ? "Unregister" : "Register Now"}   </button>
   )
 }
 
