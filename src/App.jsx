6b0165a0b37b55dd85a8301f4eb995e5548fd106
outamghartAchraf <outamghart2000@gmail.com>
2026-03-11 11:01:56 +0000

    add function for toggleRegister

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,45 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom"
 import HomePage from "./pages/HomePage"
-import { tournaments } from './data/tournamentDB';
+import { tournaments as DataTournaments } from './data/tournamentDB';
 import { useEffect, useState } from "react";
 import Loader from './components/Loader';
 import TournamentDetailsPage from './pages/TournamentDetailsPage';
 import NavBar from './components/NavBar';
+ 
 
 
 function App() {
   const [loading, setLoading] = useState(true);
+  const [tournaments, setTournaments] = useState([])
 
 useEffect(() => {
-    setTimeout(() => setLoading(false), 1000);
+    setTimeout(() => {
+      setLoading(false)
+      setTournaments(DataTournaments)
+    }, 1000);
   }, []);
 
   if (loading) return <Loader />; 
+  
+  const toggleRegistration = (tournamentsId, user) => {
 
+    setTournaments(tournaments => tournaments.map(t => {
+      if(t.id === tournamentsId) {
+        const isRegsiter = t.participants.find((p) => p.id === user.id);
+        if(isRegsiter){
+          return {
+            ...t,
+            participants: t.participants.filter((p) => p.id !== user.id)
+          }
+        }else{
+          return {
+            ...t,
+            participants: [...t.participants, user]
+          }
+
+      }
+    }))
+  }
   return (
     <>
     <BrowserRouter>
