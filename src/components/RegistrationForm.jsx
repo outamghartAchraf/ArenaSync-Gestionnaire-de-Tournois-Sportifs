a400df4f8c5090acf8acd73b41bc258975df6066
outamghartAchraf <outamghart2000@gmail.com>
2026-03-11 14:32:54 +0000

    Create states (name, team, level) in ReisterComppents

--- src/components/RegistrationForm.jsx
+++ src/components/RegistrationForm.jsx
@@ -1,5 +1,11 @@
+import { useState } from "react";
+
 function RegistrationForm() {
-    
+  const [name, setName] = useState("");
+  const [team, setTeam] = useState("");
+  const [level, setLevel] = useState("Beginner");
+  const [errors, setErrors] = useState({});
+
   return (
     <div className="bg-white/10 backdrop-blur-md rounded-2xl p-4 border border-white/20 text-white">
       <h3 className="text-xl font-bold mb-4">Register for Tournament</h3>
