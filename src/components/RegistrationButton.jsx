f056572d929b82a6a2597e2a987aeda67ea98edb
outamghartAchraf <outamghart2000@gmail.com>
2026-03-11 10:18:05 +0000

    add code for button register

---
+++ src/components/RegistrationButton.jsx
@@ -0,0 +1,12 @@
+function RegistrationButton() {
+  return (
+    <button
+     
+      className={`w-full py-3 rounded-lg font-bold transition ${"bg-green-500/80 text-white hover:bg-green-600/80 border border-green-400/50"
+      }`}
+    >
+       "Register Now"    </button>
+  )
+}
+
+export default RegistrationButton
